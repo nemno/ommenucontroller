74d6b7a87f99c6caa1b5aebf31d067dd83fd0730
Norbert Nemes <nono.nemes@gmail.com>
2013-06-30 12:22:23 +0200

    Initial Commit

---
+++ OMMenuController/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OMMenuController
+//
+//  Created by Nemes Norbert on 6/30/13.
+//  Copyright (c) 2013 Nemes Norbert. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ OMMenuController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OMMenuController
+//
+//  Created by Nemes Norbert on 6/30/13.
+//  Copyright (c) 2013 Nemes Norbert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
